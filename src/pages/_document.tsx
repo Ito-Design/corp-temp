2984c2b4d7bbc0fcc25c6e58195c57cd312b1a79
Ito-Design <nekopon839@gmail.com>
2021-12-15 17:59:38 +0900

    [Improve]コード整形

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,20 +6,20 @@ const MyDocument = () => {
   const description = 'Demo of Next.js';
 
   return (
-    <Html lang="ja-JP">
+    <Html lang='ja-JP'>
       <Head>
         {/* Change the contents of `<Head>` as needed. */}
-        <meta name="description" content={description} />
-        <meta name="theme-color" content="#333" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content={title} />
-        <meta property="og:url" content={url} />
-        <meta property="og:description" content={description} />
-        <meta property="og:site_name" content={title} />
-        <meta property="og:image" content={`${url}/ogp.png`} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="format-detection" content="telephone=no" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <meta name='description' content={description} />
+        <meta name='theme-color' content='#333' />
+        <meta property='og:type' content='website' />
+        <meta property='og:title' content={title} />
+        <meta property='og:url' content={url} />
+        <meta property='og:description' content={description} />
+        <meta property='og:site_name' content={title} />
+        <meta property='og:image' content={`${url}/ogp.png`} />
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='format-detection' content='telephone=no' />
+        <link rel='apple-touch-icon' href='/apple-touch-icon.png' />
       </Head>
       <body>
         <Main />
