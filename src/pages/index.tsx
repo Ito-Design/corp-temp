34200fa474237f554c407483cbaa6e13cf7c2cb7
Ito-Design <nekopon839@gmail.com>
2021-12-15 16:29:01 +0900

    [update]scssのインストール

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import { NextPage } from 'next';
-import styles from '../styles/Home.module.css';
+import styles from '../styles/Home.module.scss';
 
 const Home: NextPage = () => {
   return (
